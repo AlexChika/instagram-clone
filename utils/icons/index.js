16d0053e0274aa83beb26c5ce6495ea749f914a6
alex chika <mcluckey1@gmail.com>
2022-11-05 08:00:22 +0100

    building: implemented the top navbar for mobile screens

--- utils/icons/index.js
+++ utils/icons/index.js
@@ -1,4 +1,4 @@
-import PlusIcon from "./plusIcon";
+import PlusIcon from "./PlusIcon";
 import CaretDown from "./CaretDown";
 import HeartIcon from "./HeartIcon";
 import FavouritesIcon from "./FavouritesIcon";
