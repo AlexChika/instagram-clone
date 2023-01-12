12565c123f517fcdd54fb40edf8b1166920c19b2
alex chika <mcluckey1@gmail.com>
2023-01-12 10:41:22 +0100

    testing the height fourth fix

--- components/mobile/layout/index.js
+++ components/mobile/layout/index.js
@@ -1,17 +1,12 @@
 import NavBottom from "./NavBottom";
 function MobileLayout({ children, TopNav, showBottomNav = true }) {
   return (
-    //TODO check necessitty of height declaration
     <main className="h-screen">
       {/* Top Navigation */}
       {TopNav && <TopNav />}
 
       {/* Page Content */}
-      <section
-        className={`max-w-3xl mx-auto ${
-          showBottomNav ? "pb-[44px]" : "pb-[10px]"
-        }`}
-      >
+      <section className={`max-w-3xl mx-auto min-h-[100vh-44px] `}>
         {children}
       </section>
 
@@ -20,5 +15,8 @@ function MobileLayout({ children, TopNav, showBottomNav = true }) {
     </main>
   );
 }
+// ${
+//           showBottomNav ? "pb-[44px]" : "pb-[10px]"
+//         }
 
 export default MobileLayout;
