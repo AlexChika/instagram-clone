3bafe6dec403089c23817dd9ecf2ddcc8b438e30
alex chika <mcluckey1@gmail.com>
2023-01-20 05:03:48 +0100

    testing for devices failing o n select browsers

--- components/general/IconHOC.js
+++ components/general/IconHOC.js
@@ -4,38 +4,38 @@ import { App } from "../../pages/_app";
 
 const IconHOC = (Icon, path, _class) => {
   const router = useRouter();
-  const { theme } = App();
-  const [isActive, setIsActive] = useState(false);
+  // const { theme } = App();
+  // const [isActive, setIsActive] = useState(false);
 
-  const _colors = useMemo(() => {
-    return {
-      light: {
-        color: "#000000",
-        active: "#ff006f", //dark red
-      },
-      dark: {
-        color: "#ffffff",
-        active: "teal", //teal
-      },
-    };
-  }, []);
+  // const _colors = useMemo(() => {
+  //   return {
+  //     light: {
+  //       color: "#000000",
+  //       active: "#ff006f", //dark red
+  //     },
+  //     dark: {
+  //       color: "#ffffff",
+  //       active: "teal", //teal
+  //     },
+  //   };
+  // }, []);
 
-  const [colors, setColors] = useState(theme ? _colors.dark : _colors.light);
+  // const [colors, setColors] = useState(theme ? _colors.dark : _colors.light);
 
-  useEffect(() => {
-    const pathname = router.pathname;
-    const page = pathname.split("/").at(-1);
+  // useEffect(() => {
+  //   const pathname = router.pathname;
+  //   const page = pathname.split("/").at(-1);
 
-    if (page === path) {
-      setIsActive(true);
-    }
+  //   if (page === path) {
+  //     setIsActive(true);
+  //   }
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router]);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [router]);
 
-  useEffect(() => {
-    setColors(theme ? _colors.dark : _colors.light);
-  }, [theme, _colors]);
+  // useEffect(() => {
+  //   setColors(theme ? _colors.dark : _colors.light);
+  // }, [theme, _colors]);
 
   return (
     <Icon class={_class} color={"#000000"} />
