5fb4dbc66a512809273bc5e50b90c403bf189e64
alex chika <mcluckey1@gmail.com>
2023-02-08 23:58:32 +0100

    build: desktop comments modal for photo and video post complete for review

--- components/desktop/pages/post-comments/index.js
+++ components/desktop/pages/post-comments/index.js
@@ -6,6 +6,8 @@ import Footer from "components/mobile/pages/general/PostPageFooter";
 import OptionsModal from "../general/OptionsModal";
 import Comments from "components/mobile/pages/general/Comments";
 import PhotoComments from "./PhotoComments";
+import Video from "components/mobile/pages/general/Video";
+import { useRouter } from "next/router";
 
 const PostComments = ({ postData, showModal, setShowModal }) => {
   // somme calc
@@ -17,7 +19,7 @@ const PostComments = ({ postData, showModal, setShowModal }) => {
           {<CloseIcon color="white" />}
         </button>
 
-        <PhotoComments />
+        <VideoComments />
       </>
     </div>
   );
@@ -27,4 +29,171 @@ export default PostComments;
 
 {
   /* <Error title="Sorry this page does not exist.." />; */
+}
+
+function VideoComments() {
+  const router = useRouter();
+
+  // video states
+  const [muted, setMuted] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [video, setVideo] = useState(null);
+  const [id, setId] = useState(null);
+
+  const [showOptModal, setShowOptModal] = useState(false);
+  const [showShrModal, setShrOptModal] = useState(false);
+
+  const VideoWrapper = useRef(null);
+  const commentWrapper = useRef(null);
+  const [width, setWidth] = useState(null);
+
+  function mutedFn() {
+    setMuted(!muted);
+  }
+
+  function handlePlaying() {
+    setLoading(false);
+  }
+
+  function handleWaiting() {
+    setLoading(true);
+  }
+
+  //   Get video id effect
+  useEffect(() => {
+    const { id: _id } = router.query;
+    setId(_id);
+  }, [router]);
+
+  //   mute and unmute fn
+  useEffect(() => {
+    const _video = document.querySelector(`[data-id="${id}"]`);
+    if (_video) {
+      _video.muted = muted;
+    }
+  }, [muted, id, width]);
+
+  /* -------- video event listeners -------- */
+  useEffect(() => {
+    const _video = document.querySelector(`[data-id="${id}"]`);
+
+    if (!_video) return;
+    setVideo(_video);
+
+    _video.play();
+
+    _video.addEventListener("playing", handlePlaying);
+    _video.addEventListener("waiting", handleWaiting);
+
+    return () => {
+      _video.removeEventListener("playing", handlePlaying);
+      _video.removeEventListener("waiting", handleWaiting);
+    };
+  }, [id, width]);
+
+  //   effect sets the height of
+  useEffect(() => {
+    function call() {
+      if (width < 725) return;
+
+      let height = VideoWrapper.current?.getBoundingClientRect()?.height;
+
+      let commentHeight = height - 235;
+      commentWrapper.current.style.height = `${commentHeight}px`;
+    }
+
+    let interval;
+    let count = 0;
+    interval = setInterval(() => {
+      count++;
+
+      if (count > 2) clearInterval(interval);
+      call();
+    }, 500);
+  }, [width]);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <section>
+        {/* ------------ mobile screens ----------- */}
+        {width < 725 && (
+          <section className="bg-white dark:bg-black min-[725px]:hidden pb-[44px] max-w-[725px] mx-auto">
+            <Header showModal={setShowOptModal} showExtras />
+            <Video
+              id={id}
+              muted={muted}
+              muteFn={mutedFn}
+              loading={loading}
+              video={video}
+              src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            />
+            {/* <Footer showModal={setShrOptModal} /> */}
+            <Footer />
+          </section>
+        )}
+
+        {/* ---------- tablet and desktop --------- */}
+        <div className="px-5">
+          {width > 725 && (
+            <section className="bg-white dark:bg-black hidden min-[725px]:flex max-w-[815px] mx-auto mt-5 w-full border border-slate-300 dark:border-neutral-700">
+              {/* -------------- post video ------------- */}
+              <article
+                ref={VideoWrapper}
+                className="w-[58%] min-h-[450px] flex items-center justify-center self-center"
+              >
+                <Video
+                  id={id}
+                  muted={muted}
+                  muteFn={mutedFn}
+                  loading={loading}
+                  video={video}
+                  //   src="/insta-vid1.mp4"
+                  src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                />
+              </article>
+
+              {/* ------------- post details ------------ */}
+              <article className="w-[42%] border-l border-l-slate-300 dark:border-l-gray-700 ">
+                <Header showModal={setShowOptModal} showExtras />
+
+                {/* ------------ post comments ------------ */}
+                <div
+                  ref={commentWrapper}
+                  className="overflow-y-auto hide__scroll__bar px-4 py-1 border-b border-b-slate-300 dark:border-b-neutral-700"
+                >
+                  <Comments />
+                </div>
+
+                <div className="border-b border-b-slate-300 dark:border-b-neutral-700">
+                  {/* <Footer showModal={setShrOptModal} /> */}
+                  <Footer commentBox />
+                </div>
+              </article>
+            </section>
+          )}
+        </div>
+      </section>
+
+      <OptionsModal
+        showModal={showOptModal}
+        setShowModal={setShowOptModal}
+        currentPost
+      />
+
+      {/* <ShareOverlay showModal={showShrModal} setShowModal={setShrOptModal} /> */}
+    </>
+  );
 }
